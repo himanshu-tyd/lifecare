6d78e72332c5783c39a5fded81fa63a01f9613f3
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-07-11 21:00:52 +0530

    complited

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 // import dotenv from 'dotenv'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-
-
-
-})
+  root: ".",
+  build: {
+    outDir: "dist",
+  },
+  plugins: [react()],
+});
