a1d52cd1b7c142449485764719cfbd12b4d456c8
himanshu-tyd <himanshutaviyad0072@gmail.com>
2024-09-01 23:15:56 +0530

    .jsx

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "../pages/home";
+import Home from "../pages/home.jsx";
 import Login from "../pages/login";
 import Signup from "../pages/signup";
 import Doctor from "../pages/doctors/doctors";
